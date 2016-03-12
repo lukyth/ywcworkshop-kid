c3677a13d27f0e5c7f3e53db0e2e397b7cd9ac22
Kanitkorn S <k.sujautra@gmail.com>
2016-03-12 11:22:31 +0700

    Add remaining calculator function

--- calculator.js
+++ calculator.js
@@ -2,4 +2,21 @@ function add (num1, num2) {
     return num1 + num2
 }
 
-module.exports.add = add
+function minus (num1, num2) {
+    return num1 - num2
+}
+
+function multiply (num1, num2) {
+    return num1 * num2
+}
+
+function divide (num1, num2) {
+    return num1 / num2
+}
+
+module.exports = {
+  add: add,
+  minus: minus,
+  multiply: multiply,
+  divide: divide
+}
--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 var calculator = require('./calculator')
 
 console.log(calculator.add(1, 2))
+console.log(calculator.minus(1, 2))
+console.log(calculator.multiply(1, 2))
+console.log(calculator.divide(1, 2))
