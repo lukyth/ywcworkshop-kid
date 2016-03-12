b5d15d5fd7316a0dc4a2a394180483f208b2fbde
Kanitkorn S <k.sujautra@gmail.com>
2016-03-12 11:44:25 +0700

    Move module to lib/

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
-var calculator = require('./calculator')
-
-console.log(calculator.add(1, 2))
-console.log(calculator.minus(1, 2))
-console.log(calculator.multiply(1, 2))
-console.log(calculator.divide(1, 2))
+module.exports = require('./lib/')
